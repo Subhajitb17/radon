5a01800f70e6dba84da6fae04c57203dd49f2be5
Subhajitb17 <kali@kali.kali>
2022-06-07 11:52:55 +0530

    Assignment on bookSchema- MongoDb Day 1 --modified

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const bookModel= require("../models/bookModel.js")
-const UserController= require("../controllers/userController")
+const UserController= require("../controllers/bookController")
 
 
 
 router.post("/createBookList", UserController.createBookList  )
 
-router.get("/getUsersData", UserController.getBookList)
+router.get("/getBooksData", UserController.getBookList)
 
 module.exports = router;
