0683f470045495002a09cb38b190962e54a2bf69
Subhajit Bokshi <subhajitb17@gmail.com>
2022-06-15 21:19:58 +0530

    assignment auth-3 15-06-2022

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://users-open-to-all:hiPassword123@cluster0.uh35t.mongodb.net/skDB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://Subhajitb17:OgCxMXjePS5uF7pR@cluster0.4toqz.mongodb.net/Subhajit-DB-auth2", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
